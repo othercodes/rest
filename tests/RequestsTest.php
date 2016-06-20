99b510fbea46fcd7667a83f9057969f7060794ef
Unay Santisteban <usantisteban@othercode.es>
2016-06-21 01:36:58 +0200

    Request Test added.

---
+++ tests/RequestsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+class Requests extends \PHPUnit_Framework_TestCase
+{
+
+    public function testGETmethod()
+    {
+
+    }
+
+    public function testPOSTmethod()
+    {
+
+    }
+
+    public function testPUTmethod()
+    {
+
+    }
+
+    public function testPATCHmethod()
+    {
+
+    }
+
+    public function testDELETEmethod()
+    {
+
+    }
+}
